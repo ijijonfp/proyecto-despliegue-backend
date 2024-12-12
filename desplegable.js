bf52834458ea791567d99222345e7e1ce7853a76
Qimu <email>
2024-12-12 12:16:13 +0100

    Añadido menú lateral derecho

---
+++ desplegable.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $("#botonDesplegable").click(function() {
+        if ($("#menuDesplegable").css("right") == "0px") {
+
+            $("#menuDesplegable").css("right", "-21%"); 
+        } else {
+            $("#menuDesplegable").css("right", "0");
+        }
+        
+    });
+
+    $("#homeSideBar").click(function(){
+        $("#menuDesplegable").css("right", "-21%");
+    })
+
+    $("#detailsSideBar").click(function(){
+        $("#menuDesplegable").css("right", "-21%");
+    })
+
+    $("#storySideBar").click(function(){
+        $("#menuDesplegable").css("right", "-21%");
+    })
+
+    $("#contactSideBar").click(function(){
+        $("#menuDesplegable").css("right", "-21%");
+    })
+
+});
