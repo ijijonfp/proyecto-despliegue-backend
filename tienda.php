0ae89498931a0ed5bd89557f9178a84b39b0ba48
Carla <carluchi012002@gmail.com>
2025-01-16 10:46:03 +0100

    Creación estructura CSS de la rama tienda

--- tienda.php
+++ tienda.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
     <link rel="stylesheet" href="./css/normalize.css">
-    <link rel="stylesheet" href="./css/tienda.css">
+    <link rel="stylesheet" href="css/tienda.css">
 </head>
 <body>
     <section class="products-container">
