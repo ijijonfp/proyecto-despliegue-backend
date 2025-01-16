8bf5eed226952b9d961ac05555baeb0fe3dbe378
Carla <carluchi012002@gmail.com>
2025-01-16 10:06:03 +0100

    creacion estructura tienda de vinilos

--- php/tienda.php
+++ php/tienda.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
-    
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/tienda.css">
 </head>
 <body>
-    
+    <section class="products-container">
+        <div class="product-details">
+            <div class="image-container"></div>
+            <div class="text-container"></div>
+        </div>
+    </section>
 </body>
 </html>
