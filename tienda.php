a82510cf6ab48d871d0b4ddf065bba09895c38cb
Carla <carluchi012002@gmail.com>
2025-01-23 09:00:44 +0100

    Creación de css y estructura html de la rama tienda

--- tienda.php
+++ tienda.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,26 @@
     <link rel="stylesheet" href="./css/normalize.css">
     <link rel="stylesheet" href="css/tienda.css">
 </head>
+
 <body>
     <section class="products-container">
+        <!-- 1 -->
         <div class="product-details">
-            <div class="image-container"></div>
-            <div class="text-container"></div>
+            <div class="image-container">
+                <div class="vinil-image">
+                    <img src="" alt="Imagen">
+                </div>
+            </div>
+            <div class="text-container">
+                <div class="info-container">
+                    <p>Nombre:</p>
+                    <p>Descripción:</p>
+                    <p>Precio:</p>                  
+                </div>
+            </div>
+            </div>
         </div>
     </section>
 </body>
+
 </html>
