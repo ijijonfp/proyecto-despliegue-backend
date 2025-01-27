c490fdc5b32a6c9099790ec4fa311632ed27a3e1
Qimu <email>
2025-01-27 21:14:23 +0100

    añadir simbolos visibilidad y mejor espaciado entre vinilos

--- php/tablaCatalogo.php
+++ php/tablaCatalogo.php
@@ -2,7 +2,17 @@
 
 include_once("configuracion.php");
 
-$sql = "SELECT imagenVinilo, idVinilo, tituloVinilo, idBanda, descripcionVinilo, precioVinilo, visible FROM vinilos";
+$sql = "
+    SELECT 
+        v.imagenVinilo, 
+        v.idVinilo, 
+        v.tituloVinilo, 
+        b.nombreBanda,  
+        v.precioVinilo, 
+        v.visible 
+    FROM vinilos v
+    JOIN banda b ON v.idBanda = b.idBanda
+";
 
 $result = $conn->query($sql);
 
@@ -17,14 +27,18 @@ if ($result->num_rows > 0) {
         foreach ($row as $key => $value) {
             if ($key === 'imagenVinilo') {
                 $tabla .= "<td><div class='vinylImage' style='background-image: url(" . htmlspecialchars($value) . ");'></div></td>";
+                $tabla .= "<div class='bodyRow'>";
             } elseif ($key === 'idVinilo') {
                 $tabla .= "<td style='display: none;'>" . htmlspecialchars($value) . "</td>";
             } elseif ($key === 'visible') {
                 if ($value === 'True') {
-                    $tabla .= "<td><a href='ocultarVinilo.php?idVinyl=" . $row['idVinilo'] . "'>O</a></td>";
+                    $tabla .= "<td><a href='ocultarVinilo.php?idVinyl=" . $row['idVinilo'] . "'><div class='visibleImg'></div></a></td>";
                 } else {
-                    $tabla .= "<td><a href='desocultarVinilo.php?idVinyl=" . $row['idVinilo'] . "'>Ø</a></td>";
+                    $tabla .= "<td><a href='desocultarVinilo.php?idVinyl=" . $row['idVinilo'] . "'><div class='invisibleImg'></div></a></td>";
                 }
+            } elseif ($key === 'precioVinilo') {
+                // Aquí añadimos el símbolo de euro al precio
+                $tabla .= "<td>" . htmlspecialchars($value) . " €</td>";
             } else {
                 $tabla .= "<td>" . htmlspecialchars($value) . "</td>";
             }
