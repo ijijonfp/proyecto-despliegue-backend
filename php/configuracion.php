ab964309ff4ba3fb8c808724628505fb0b7890c9
Isaac Prueba cambio <ijijon@campuscamarafp.com>
2025-01-30 00:06:16 +0100

    creo rama nueva para configurar datos de conexion a bd para railweay en archivo config

--- php/configuracion.php
+++ php/configuracion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "OldVinyl";
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');  
+$password = getenv('DB_PASSWORD');  
+$dbname = getenv('DB_NAME');  
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
