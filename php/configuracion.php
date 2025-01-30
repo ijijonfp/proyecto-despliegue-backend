56d31f816529b325f55ae4789d3ef2d7df1b7906
Isaac Prueba cambio <ijijon@campuscamarafp.com>
2025-01-30 01:30:20 +0100

    modifico datos de la conexion a base de datos en el archivo config

--- php/configuracion.php
+++ php/configuracion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = getenv('DB_HOST');
-$username = getenv('DB_USER');  
-$password = getenv('DB_PASSWORD');  
-$dbname = getenv('DB_NAME');  
+$servername = getenv('MYSQLHOST');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$dbname = getenv('MYSQLDATABASE');
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
